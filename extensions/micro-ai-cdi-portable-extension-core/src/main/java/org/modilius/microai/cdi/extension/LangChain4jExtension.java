ba04c79215c62960e2f8fe7ebf302a5a3381f0d4
Yann Blazart <yann.blazart@bycode.fr>
2024-06-23 16:33:27 +0200

    riviera2024/step007_portable_base-example-with-stereotype:

--- extensions/micro-ai-cdi-portable-extension-core/src/main/java/org/modilius/microai/cdi/extension/LangChain4jExtension.java
+++ extensions/micro-ai-cdi-portable-extension-core/src/main/java/org/modilius/microai/cdi/extension/LangChain4jExtension.java
@@ -1,14 +1,32 @@
 package org.modilius.microai.cdi.extension;
 
+import jakarta.enterprise.event.Observes;
+import jakarta.enterprise.inject.spi.BeforeBeanDiscovery;
 import jakarta.enterprise.inject.spi.Extension;
+import jakarta.enterprise.inject.spi.ProcessAnnotatedType;
+import jakarta.enterprise.inject.spi.WithAnnotations;
+import org.jboss.logging.Logger;
+import org.modilius.microai.cdi.extension.spi.RegisterAIService;
 
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class LangChain4jExtension implements Extension {
+    private static final Logger LOGGER = Logger.getLogger(LangChain4jExtension.class);
     private static final Set<String> detectedAIServicesDeclaredInterfaces = new HashSet<>();
     public static Set<String> getDetectedAIServicesDeclaredInterfaces() {
         return detectedAIServicesDeclaredInterfaces;
     }
 
+
+    public void makeRegisterAIasStereotype(@Observes BeforeBeanDiscovery bbd) {
+        LOGGER.info("makeRegisterAIasStereotype");
+        bbd.addStereotype(RegisterAIService.class);
+    }
+
+
+    <T> void processAnnotatedType(@Observes @WithAnnotations({RegisterAIService.class}) ProcessAnnotatedType<T> pat) {
+        LOGGER.info("processAnnotatedType "+pat.getAnnotatedType().getJavaClass().getName());
+    }
 }
