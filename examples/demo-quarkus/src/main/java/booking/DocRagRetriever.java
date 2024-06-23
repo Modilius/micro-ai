8946a0665164408a6e7b712c5567be0c9f9feec9
Yann Blazart <yann.blazart@bycode.fr>
2024-06-23 19:08:20 +0200

    riviera2024/step010 quarkus

---
+++ examples/demo-quarkus/src/main/java/booking/DocRagRetriever.java
@@ -0,0 +1,34 @@
+package booking;
+
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class DocRagRetriever {
+
+    @Inject
+    EmbeddingStore<TextSegment> embeddingStore;
+
+    @Inject
+    EmbeddingModel embeddingModel;
+
+    @Produces
+    ContentRetriever contentRetriever;
+
+    @PostConstruct
+    void init() {
+        contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(3)
+                .minScore(0.6)
+                .build();
+    }
+}
---
+++ examples/demo-quarkus/src/main/java/booking/FraudResponse.java
@@ -0,0 +1,14 @@
+package booking;
+
+import lombok.Data;
+
+import java.util.List;
+
+// Warning: Java Record not supported by Google JSON (used by LangChain4J)
+@Data
+public class FraudResponse {
+    private String customerName; 
+    private String customerSurname; 
+    private boolean fraudDetected;
+    private  List<String> bookingIds;
+}
